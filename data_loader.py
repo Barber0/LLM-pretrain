e3ace3b72d19c738458ea7c6b19c11426477fe82
Barber0 <simonfangzilin@outlook.com>
2023-07-08 00:58:44 +0800

    base arch

---
+++ data_loader.py
@@ -0,0 +1,29 @@
+from datasets import load_from_disk
+
+
+def SelfInstructLoader(ds_path, batch_size=5):
+    data = load_from_disk(ds_path)
+
+    def _iter():
+        batch = []
+        for i, v in enumerate(data['train']):
+            batch.append(f'''{v['prompt']} {v['completion']}''')
+            if i > 0 and i % batch_size == 0:
+                out = batch
+                batch = []
+                yield out
+    return _iter
+
+
+def BcdsLoader(ds_path, batch_size=5):
+    data = load_from_disk(ds_path)
+
+    def _iter():
+        batch = []
+        for i, v in enumerate(data['train']):
+            batch.append(v['text'])
+            if i > 0 and i % batch_size == 0:
+                out = batch
+                batch = []
+                yield out
+    return _iter
---
+++ train_epoch.py
@@ -0,0 +1,84 @@
+from time import time
+
+
+def train_epoch(
+    ep,
+    model_eng,
+    start_batch,
+    batch_period,
+    md_path,
+    md_tag,
+    mount_dir,
+    START_SIGN,
+    END_SIGN,
+    tkn,
+    data_loader,
+    writer,
+):
+    for i, batch in enumerate(data_loader()):
+        if i < start_batch:
+            continue
+
+        x_encoded = tkn.batch_encode_plus(
+            [f'{START_SIGN} {line}' for line in batch],
+            max_length=512,
+            padding=True,
+            truncation=True,
+            return_tensors='pt'
+        )
+        input_ids, x_attn_mask = x_encoded['input_ids'], x_encoded['attention_mask']
+
+        target_ids = tkn.batch_encode_plus(
+            [f'{line} {END_SIGN}' for line in batch],
+            max_length=512,
+            padding=True,
+            truncation=True,
+            return_tensors='pt'
+        )['input_ids']
+
+        input_ids = input_ids.cuda()
+        target_ids = target_ids.cuda()
+
+        loss = model_eng(input_ids, target_ids)
+        model_eng.backward(loss)
+        model_eng.step()
+
+        try:
+            writer.add_scalar('Train Loss', loss, i)
+        except Exception as e:
+            print(f'\r{e}')
+
+        total_loss += loss
+        period_loss += loss
+
+        if i > 0 and i % batch_period == 0:
+            time_period = time() - stime
+            avg_ntokens = x_attn_mask.sum() / x_attn_mask.size(0)
+            pad_token_len = x_attn_mask.size(-1)
+            print(f'\rbatch: {i}, time: {time_period}, avg_ntokens: {avg_ntokens},'
+                  f' loss: {period_loss}, per_loss: {period_loss / batch_period},'
+                  f' pad_len: {pad_token_len}')
+            writer.flush()
+
+            try:
+                if i % 200 == 0:
+                    save_chkpt(str(i), model_eng, mount_dir, md_path, md_tag)
+            except Exception as e:
+                print(e)
+
+            period_loss = 0
+            stime = time()
+
+    save_chkpt(f'epoch-{ep}', model_eng, mount_dir, md_path, md_tag)
+
+
+def save_chkpt(
+    name,
+    model_eng,
+    mount_dir,
+    md_path,
+    md_tag
+):
+    model_eng.save_checkpoint(md_path, tag=md_tag)
+    with open(f'{mount_dir}/progress.txt', 'w') as f:
+        f.write(name)
