db140cfa56f2e1ad2635a1bb93990757ae70f776
Barber0 <simonfangzilin@outlook.com>
2023-07-13 11:15:42 +0800

    unstable add: grad scaler

--- pretrain.py
+++ pretrain.py
@@ -7,9 +7,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 from myllm_model import MyModel
 from data_loader import DataLoader
-from torch.cuda.amp import autocast, GradScaler
-from torch.optim import AdamW
-import logging
+from loss_scaler import LossScaler
+from torch.optim import SGD
+from utils import build_logger
 
 SAVED_MODEL_TAG = 'module'
 SAVED_OPT_TAG = 'opt'
@@ -24,13 +24,9 @@ def prepare_tokenizer(tkn_path, added_tokens=['[END]']):
 
 
 def run(args):
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s [%(levelname)s] %(message)s',
-        handlers=[
-            logging.FileHandler(args.my_log),
-            logging.StreamHandler()
-        ]
+    logger = build_logger(
+        name='pretrain',
+        log_filename=args.my_log
     )
 
     tkn, VOCAB_SIZE = prepare_tokenizer(args.tkn_path)
@@ -45,7 +41,7 @@ def run(args):
         max_len=args.max_len
     ).cuda().half()
 
-    opt = AdamW(model.parameters(), lr=args.lr)
+    opt = SGD(model.parameters(), lr=args.lr)
 
     os.makedirs(args.save_home, exist_ok=True)
     if args.load_home is not None:
@@ -56,14 +52,10 @@ def run(args):
         if os.path.exists(load_opt_path):
             opt.load_state_dict(torch.load(load_opt_path))
 
-    scaler = GradScaler()
-
-    num_micro_batch = args.batch_size // args.micro_batch
-
     writer = SummaryWriter(log_dir=args.log_path)
+    scaler = LossScaler(logger=logger)
 
     period_loss = 0
-
     stime = time()
     for bidx, batch in enumerate(data_loader()):
         if bidx < args.start_batch:
@@ -78,56 +70,53 @@ def run(args):
         )
         base_ids, x_attn_mask = x_encoded['input_ids'], x_encoded['attention_mask']
 
-        input_ids = base_ids[..., :-1].cuda()
-        target_ids = base_ids[..., 1:].cuda()
+        input_ids = base_ids[..., :-1]
+        target_ids = base_ids[..., 1:]
 
         micro_batch_start = 0
         real_batch_size = len(batch)
 
         batch_loss = 0
-        scaled_batch_loss = 0
 
+        opt.zero_grad()
+
+        num_micro_batch = args.batch_size // args.micro_batch
         base_row_idx = bidx*args.batch_size
         while micro_batch_start < real_batch_size:
             micro_batch_end = micro_batch_start + args.micro_batch
             row_idx = base_row_idx + micro_batch_end
 
             loss = model.forward(
-                input_ids[micro_batch_start:micro_batch_end],
-                target_ids[micro_batch_start:micro_batch_end],
+                x=input_ids[micro_batch_start:micro_batch_end].cuda(),
+                y=target_ids[micro_batch_start:micro_batch_end].cuda(),
+                num_micro_batch=num_micro_batch
             )
-            scaled_loss = scaler.scale(loss)
-            scaled_loss.backward()
+            if scaler.nan_or_inf(loss):
+                scaler.update(True)
+                continue
 
+            loss.backward()
+            batch_loss += loss
             micro_batch_start = micro_batch_end
 
-            if not torch.isnan(loss):
-                batch_loss += loss
-
-            if not torch.isnan(scaled_loss):
-                scaled_batch_loss += scaled_loss
-
-        scaler.unscale_(opt)
-        scaler.step(opt)
-        scaler.update()
-        opt.zero_grad()
+        if scaler.step(model, opt):
+            continue
 
         try:
             writer.add_scalar('Train Loss', batch_loss, row_idx)
-            writer.add_scalar('Scaled Train Loss', scaled_batch_loss, row_idx)
         except Exception as e:
-            logging.warn('batch: %d, tensorboard error: %s', bidx, e)
+            logger.warn('batch: %d, tensorboard error: %s', bidx, e)
 
-        period_loss += scaled_batch_loss
+        period_loss += batch_loss
 
         next_bidx = bidx + 1
         if next_bidx % args.batch_period == 0:
             time_period = time() - stime
             avg_ntokens = x_attn_mask.sum() / x_attn_mask.size(0)
             pad_token_len = x_attn_mask.size(-1)
-            logging.info(
-                'batch: %d, time: %.2f, ntokens: %.2f/%d, loss: %.2f',
-                bidx, time_period, avg_ntokens, pad_token_len, period_loss/args.batch_period
+            logger.info(
+                'batch: %d, time: %.2f, ntokens: %.2f/%d, loss: %f',
+                next_bidx, time_period, avg_ntokens, pad_token_len, period_loss/args.batch_period
             )
             writer.flush()
 
@@ -136,7 +125,8 @@ def run(args):
                     next_bidx,
                     model,
                     opt,
-                    args
+                    args,
+                    logger
                 )
 
             period_loss = 0
@@ -144,10 +134,11 @@ def run(args):
 
 
 def save_chkpt(
-    next_bidx,
+    bidx,
     model,
     opt,
-    args
+    args,
+    logger
 ):
     try:
         if os.path.exists(args.save_home):
@@ -157,9 +148,9 @@ def save_chkpt(
                        f'{args.save_home}/opt-{args.model_name}.pt')
 
             with open(f'./progress.txt', 'w') as f:
-                f.write(str(next_bidx))
+                f.write(str(bidx))
     except Exception as ex:
-        logging.warn('batch: %d, save state failed: %s', bidx, ex)
+        logger.warn('batch: %d, save state failed: %s', bidx, ex)
 
 
 if __name__ == '__main__':
@@ -183,7 +174,7 @@ if __name__ == '__main__':
 
     arg_parser.add_argument('--batch_size', default=50)
     arg_parser.add_argument('--micro_batch', default=10)
-    arg_parser.add_argument('--lr', default=7e-5)
+    arg_parser.add_argument('--lr', default=1e-2)
 
     arg_parser.add_argument('--max_len', default=1024)
     arg_parser.add_argument('--d_model', default=256)
