5cfed848ede46c35f5ce031c19db3de97501afe4
Barber0 <simonfangzilin@outlook.com>
2023-07-18 03:58:08 +0800

    update: remove end sign in pretrain

--- pretrain.sh
+++ pretrain.sh
@@ -5,7 +5,6 @@ deepspeed --num_gpus=1 pretrain.py \
     --n_head=32 \
     --n_block=24 \
     --batch_period=50 \
-    --start_batch=501 \
     --data_path=/root/autodl-tmp/wiki/ \
     --ckpt=/root/autodl-tmp/rope-llm/ \
     --model_name=main
