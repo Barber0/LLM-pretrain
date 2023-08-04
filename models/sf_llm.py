fix: deepspeed complex broadcast

models/sf_llm.py
@@ -170,13 +170,13 @@ class SFEmbedding(nn.Embedding):
         pad_token_id: int,
         n_layers: int,
         base_mask: torch.Tensor,
-        get_emb_table_fn: RoPE.EmbTableGetterType
+        get_phase_table_fn: RoPE.PhaseTableGetterType
     ):
         super().__init__(num_embeddings, embedding_dim)
         self.pad_token_id = pad_token_id
         self.n_layers = n_layers
         self.base_mask = base_mask
-        self.get_emb_table_fn = get_emb_table_fn
+        self.get_phase_table_fn = get_phase_table_fn
 
     def forward_with_prefix(
         self,
@@ -190,8 +190,8 @@ class SFEmbedding(nn.Embedding):
         seq_len = input.size(-1)
         end_idx = start_idx+seq_len
 
-        emb_table_k = self.get_emb_table_fn(end_idx, 0)
-        emb_table_q = emb_table_k if start_idx == 0 else self.get_emb_table_fn(
+        emb_table_k = self.get_phase_table_fn(end_idx, 0)
+        emb_table_q = emb_table_k if start_idx == 0 else self.get_phase_table_fn(
             seq_len, start_idx)
 
         mask = None
@@ -285,7 +285,7 @@ class SFLLM(nn.Module):
             self.pad_token_id,
             self.args.n_layers,
             self.create_mask(self.args.max_len * self.args.ext_factor),
-            self.rope_emb.get_embedding_table
+            self.rope_emb.get_phase_table
         )
 
         self.blocks = nn.ModuleList([

position_encoding2/abstract_rope.py
@@ -7,7 +7,7 @@ from torch import Tensor
 
 
 class RoPE(nn.Module):
-    EmbTableGetterType = Callable[[int, int], Tensor]
+    PhaseTableGetterType = Callable[[int, int], Tensor]
     RoPEFunctionType = Callable[[Tensor, Tensor, int, int], Tensor],
 
     def __init__(
@@ -25,9 +25,9 @@ class RoPE(nn.Module):
         self._theta = theta
         self._max_len = max_len
         self._ext_factor = ext_factor
-        self._ensure_emb_table()
+        self._ensure_phase_table()
 
-    def _ensure_emb_table(self):
+    def _ensure_phase_table(self):
         freqs = 1.0 / (self._theta ** (
             torch.arange(0, self._hidden_states, 2)
             [: self._hidden_states // 2].float() / self._hidden_states))
@@ -39,21 +39,17 @@ class RoPE(nn.Module):
             ) / self._interpolate_factor
         else:
             pos_ids = torch.arange(0, self._max_len*self._ext_factor)
-        phase = torch.outer(pos_ids, freqs)
-        self.emb_table = nn.parameter.Parameter(
-            self._polar(torch.ones_like(phase), phase), requires_grad=False)
 
-    def _polar(self, mag, phase):
-        return torch.polar(mag, phase)
+        self.phase = nn.parameter.Parameter(
+            torch.outer(pos_ids, freqs), requires_grad=False)
 
-    def get_embedding_table(self, seq_len: int, start_idx: int):
-        self._ensure_emb_table()
-        return self.emb_table[start_idx:start_idx+seq_len]
+    def get_phase_table(self, seq_len: int, start_idx: int):
+        return self.phase[start_idx:start_idx+seq_len]
 
     def apply_rotary(
         self,
         x: torch.Tensor,
-        emb_table: torch.Tensor,
+        phase: torch.Tensor,
         seq_len_dim_idx: int = 2,
         head_dim_idx: int = 3
     ):

position_encoding2/complex_rope.py
@@ -8,13 +8,17 @@ class ComplexRoPE(RoPE):
     def apply_rotary(
         self,
         x: Tensor,
-        emb_table: Tensor,
+        phase: Tensor,
         seq_len_dim_idx: int = 2,
         head_dim_idx: int = 3
     ):
         x_ = self._real_to_complex(x.float())
         emb_table = self._reshape_as_broadcast(
-            emb_table, x_, seq_len_dim_idx, head_dim_idx)
+            torch.polar(torch.ones_like(phase), phase),
+            x_,
+            seq_len_dim_idx,
+            head_dim_idx
+        )
         return self._complex_to_real(emb_table * x_).type_as(x)
 
     @staticmethod

position_encoding2/simulated_rope.py
@@ -4,16 +4,20 @@ from .complex_rope import ComplexRoPE
 
 
 class SimulatedRoPE(ComplexRoPE):
-    def _polar(self, magnitude, phase):
+    def polar(self, magnitude, phase):
         real_part = magnitude * torch.cos(phase)
         imag_part = magnitude * torch.sin(phase)
         real_tensor = torch.stack((real_part, imag_part), dim=-1)
         return real_tensor
 
-    def apply_rotary(self, x: torch.Tensor, emb_table: torch.Tensor, seq_len_dim_idx: int = 2, head_dim_idx: int = 3):
+    def apply_rotary(self, x: torch.Tensor, phase: torch.Tensor, seq_len_dim_idx: int = 2, head_dim_idx: int = 3):
         x_ = self._real_to_complex(x)
         emb_table = self._reshape_as_broadcast(
-            emb_table, x_, seq_len_dim_idx, head_dim_idx)
+            self.polar(torch.ones_like(phase), phase),
+            x_,
+            seq_len_dim_idx,
+            head_dim_idx
+        )
         return self._complex_to_real(self._complex_multiply_on_2d_real(emb_table, x_))
 
     @staticmethod